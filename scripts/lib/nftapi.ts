a2bd2577519b65559ee79d0faf0bcc89559509cb
chaeyk <chaeyk@neowiz.com>
2023-02-27 17:17:45 +0900

    resyncMetadata parameter  추가

--- scripts/lib/nftapi.ts
+++ scripts/lib/nftapi.ts
@@ -3,7 +3,7 @@ import https from 'https';
 export function getMetadata(chainId: string, tokenAddress: string, tokenId: string): Promise<any> {
   return new Promise((resolve, reject) => {
     try {
-      https.get(`https://nft.api.infura.io/networks/${chainId}/nfts/${tokenAddress}/tokens/${tokenId}`, {
+      https.get(`https://nft.api.infura.io/networks/${chainId}/nfts/${tokenAddress}/tokens/${tokenId}?resyncMetadata=true`, {
         headers: {
           authorization: 'Basic ' + Buffer.from(process.env.INFURA_APIKEY + ":" + process.env.INFURA_SECRET).toString('base64'),
         },
